f9b8f6a8b05436f642046c1283bcecbc36e7ff27
narges <jatages@gmail.com>
2021-03-11 17:43:34 +0330

    Add getArea function for rectangular shapes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import Rectangle
+
 print("Welcom\n What do you want to make?")
 print("1)Circle\n2)Square\n3)Rectangle\n4)Triangle")
 shap = int(input())
+
+if shap == 3:
+    side1 = int(input())
+    side2 = int(input())
+    my_rect = Rectangle.Rectangle(side1,side2)
+    print("Rectangle area = ",my_rect.getArea())
+    # print("Rectangle perimeter = ",my_rect.getPerimeter())
